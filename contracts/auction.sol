31760413beaed881166332813d3c779f5fea98c1
marvin-heiden <marvin.heiden@senacor.com>
2022-02-06 17:50:13 +0100

    added lock time and escrow functionality in case of complaint

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -4,16 +4,20 @@ pragma solidity ^0.8.4;
 contract Auction {
     // static
     address public owner;
+    address public moderator;
     string public description;
     uint public startBlock;
     uint public endBlock;
+    uint public locktime;
 
     // state
     bool public canceled;
+    bool public complaintFiled;
+    bool public complaintConfirmed;
     address public highestBidder;
     uint public highestBid;
     mapping(address => uint256) public fundsByBidder;
-    bool ownerHasWithdrawn;
+    bool paymentFulfilled;
 
     // logs
     event LogBid(address bidder, uint bid, address highestBidder, uint highestBid);
@@ -28,6 +32,16 @@ contract Auction {
 
     modifier onlyNotOwner {
         require (msg.sender != owner);
+        _;
+    }
+
+    modifier onlyModerator {
+        require(msg.sender == moderator);
+        _;
+    }
+
+    modifier onlyHighestBidder {
+        require(msg.sender == highestBidder);
         _;
     }
 
@@ -46,16 +60,29 @@ contract Auction {
         _;
     }
 
-    constructor(address _owner, uint _startBlock, uint _endBlock, string memory _description) {
+    modifier onlyOnComplaint {
+        require(complaintFiled);
+        _;
+    }
+
+    modifier onlyDuringLocktime {
+        require (block.number >= endBlock && block.number < endBlock + locktime);
+        _;
+    }
+
+    constructor(address _owner, uint _startBlock, uint _endBlock, string memory _description, uint _locktime) {
         require(_startBlock < _endBlock);
         require(_startBlock >= block.number);
         require(_owner != address(0));
+        require(_owner != msg.sender);
         require(keccak256(abi.encodePacked(_description)) != keccak256(abi.encodePacked("")));
 
+        moderator = msg.sender;
         owner = _owner;
         startBlock = _startBlock;
         endBlock = _endBlock;
         description = _description;
+        locktime = _locktime;
     }
 
     function placeBid()
@@ -106,6 +133,36 @@ contract Auction {
         return endBlock - block.number;
     }
 
+    function fileComplaint()
+        public
+        onlyDuringLocktime
+        onlyHighestBidder
+        returns (bool success)
+    {
+        complaintFiled = true;
+        return true;
+    }
+
+    function confirmComplaint()
+        public
+        onlyModerator
+        onlyOnComplaint
+        returns (bool success)
+    {
+        complaintConfirmed = true;
+        return true;
+    }
+
+    function rejectComplaint()
+        public
+        onlyModerator
+        onlyOnComplaint
+        returns (bool success)
+    {
+        complaintFiled = false;
+        return true;
+    }
+
     function withdraw()
         public
         returns (bool success)
@@ -118,14 +175,22 @@ contract Auction {
             withdrawalAmount = fundsByBidder[withdrawalAccount];
 
         } else if (block.number >= endBlock){ // auction terminated
-            if (msg.sender == owner) {
+            if (msg.sender == owner && 
+            block.number >= endBlock + locktime && 
+            complaintFiled == false) {
                 withdrawalAccount = highestBidder;
                 withdrawalAmount = highestBid;
-                ownerHasWithdrawn = true;
+                paymentFulfilled = true;
 
             } else if (msg.sender == highestBidder) {
-                withdrawalAmount = 0;
-
+                if (complaintConfirmed == true){
+                    withdrawalAccount = highestBidder;
+                    withdrawalAmount = highestBid;
+                    paymentFulfilled = true;
+                } else {
+                    withdrawalAmount = 0;
+                }
+                
             } else {
                 withdrawalAccount = msg.sender;
                 withdrawalAmount = fundsByBidder[withdrawalAccount];
